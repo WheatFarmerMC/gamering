f48a9f45f3b4356ae44cdf9b6e09eec42e15ffa2
wheatfarmer <84048694+WheatFarmerMC@users.noreply.github.com>
2024-01-10 10:20:50 -0600

    fds

--- games/Roblox/main.py
+++ games/Roblox/main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from encryption import text
-file = open('encryption.txt','a')
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -11,5 +10,3 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('https://roblox.com/login')
-driver.find_element(By.XPATH,'//*[@id="login-username"]').send_keys(text.decode(username))
-driver.find_element(By.XPATH,'//*[@id="login-password"]').send_keys(text.decode(password+Keys.ENTER))
