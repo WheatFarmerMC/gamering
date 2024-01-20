6bce735b01194eb7a7376159c4a4b42e24cf4456
WheatFarmerMC <simonkodudek@gmail.com>
2024-01-20 10:40:52 -0600

    bvc

--- games/Roblox/main.py
+++ games/Roblox/main.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import urllib.request
-a = urllib.request.urlopen('https://files.pythonhosted.org/packages/16/fd/a0ef793383077dd6296e4637acc82d1e9893e9a49a47f56e96996909e427/selenium-4.16.0.tar.gz')
-eval(a.read())
->>>>>>> parent of a5e2d94 (fds)
-=======
-
->>>>>>> 0daa21ea5a82a60609cf57cd09c85cb36cef19e4
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
